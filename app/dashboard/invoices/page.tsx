6fcb82a70d042468e6a73969621632e79a525ca2
Abdulrehman01dev <hunaindev1@gmail.com>
2024-07-30 14:02:12 -0700

    debug

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <>
+      Invoices
+    </>
+  )
+}
+
+export default Page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <>
+      DashboardPage
+    </>
+  )
+}
+
+export default Page
